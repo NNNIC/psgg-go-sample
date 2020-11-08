fef5f4812eb3eb064f98728c7e49f7da540e41f9
Naruto Nishizawa <n.nishizawa@mbr.nifty.com>
2020-11-08 18:25:25 +0900

    no message

--- test/3/TestControl.go
+++ test/3/TestControl.go
@@ -5,126 +5,61 @@ import (
 )
 
 func main() {
-	run()
+	testControl()
 }
 
-var mCurfunc = ""
-var mNextfunc = ""
-var mNoWait = false
-var mFuncMap map[string]func(bool)
-
-func initialize() {
-	mCurfunc := ""
-	mNextfunc := ""
-	mFuncMap := make(map[string]func(bool))
-	mFuncMap["S_START"] = sSTART
-	mFuncMap["S_END"] = sEND
-}
-
-func update() {
-	for true {
-		var bFirst = false
-		mNoWait := false
-		if mNextfunc == "" {
-			mCurstate := mNextfunc
-			mNextfunc := ""
-			bFirst := true
-		}
-		if mCurfunc != "" {
-			mFuncMap[mCurfunc](bFirst)
-		}
-		if mNoWait == false {
-			break
+func testControl() {
+	var curfunc = ""
+	var nextfunc = ""
+	var S_START = func(bFirst bool) {
+		if bFirst {
+			fmt.Println("S_START")
+			nextfunc = "S_WORK"
 		}
 	}
-}
+	var sEnd = func(bFirst bool) {
+		if bFirst {
+			fmt.Println("S_END")
+		}
+	}
+	var sWork = func(bFirst bool) {
+		nextfunc = "S_END"
+	}
+	var funcmap = make(map[string]func(bool))
+	funcmap["S_START"] = S_START
+	funcmap["S_END"] = sEnd
+	funcmap["S_WORK"] = sWork
 
-func gotoState(st string) {
-	mNextfunc = st
-}
-func checkState(st string) bool {
-	return st == mCurfunc
-}
-func hasNextState() bool {
-	return mNextfunc != ""
-}
-func noWait() {
-	mNoWait := true
-}
-func GoSubState(_, _ string) {
-	// if m_callstack_level >= MAX_CALL_STACK-1 {
-	//     print("CALL STACK OVER FLOW")
-	// }
-	// else {
-	//     m_callstack[m_callstack_level] = next;
-	//     m_callstack_level += 1
-	//     Goto(sub)
-	// }
-}
-func returnState() {
-	// var st: String? = nil;
-	// if m_callstack_level <= 0 {
-	//     print("CALL STACK UNDER FLOW")
-	// }
-	// else {
-	//     m_callstack_level -= 1
-	//     st = m_callstack[m_callstack_level]
-	//     Goto(st)
-	// }
+	nextfunc = "S_START"
+
+	for curfunc != "S_END" {
+		var bFirst = false
+		if nextfunc != "" {
+			curfunc = nextfunc
+			nextfunc = ""
+			bFirst = true
+		}
+		if curfunc != "" {
+			funcmap[curfunc](bFirst)
+		}
+	}
 }
 
 // state
 //[STATEGO OUTPUT START] indent(0) $/./$
 //             psggConverterLib.dll converted from psgg-file:TestControl.psgg
 
-/*
-    S_END
-*/
-func sEND( bFirst  bool ) {
-     // end of state machine
-}
-/*
-    S_HELLOWORLD
-*/
-func sHELLOWORLD( bFirst  bool ) {
-    if bFirst {
-        fmt.Println("Hello World.")
-    }
-    if !hasNextState() {
-        gotoState("S_NEWERA");
-    }
-}
-/*
-    S_NEWERA
-*/
-func sNEWERA( bFirst  bool ) {
-    if bFirst {
-        fmt.Println("We are in the visual programming era.")
-    }
-    if !hasNextState() {
-        gotoState("S_END");
-    }
-}
-/*
-    S_START
-*/
-func sSTART( bFirst  bool ) {
-    gotoState("S_HELLOWORLD")
-    noWait()
-}
-
-
 //[STATEGO OUTPUT END]
 
 //Run
 func run() {
-	fmt.Println("RUN!")
-	initialize()
-	gotoState("S_START")
-	for checkState("S_END") == false {
-		update()
-	}
-	fmt.Println("END OF RUN")
+	// fmt.Println("RUN!")
+	// initialize()
+	// gotoState("S_START")
+	// for checkState("S_END") == false {
+	// 	update()
+	// }
+	// fmt.Println("END OF RUN")
 }
 
 //let sm = TestControl()
