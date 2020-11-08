7f6dfef5b06f22951d7f6e5871f8cfbe4252b5cc
nn4343 <gea01441@nifty.com>
2020-11-08 22:33:47 +0900

    no message

--- test/3/TestControl.go
+++ test/3/TestControl.go
@@ -11,24 +11,116 @@ func main() {
 func testControl() {
 	var curfunc = ""
 	var nextfunc = ""
-	var S_START = func(bFirst bool) {
-		if bFirst {
-			fmt.Println("S_START")
-			nextfunc = "S_WORK"
-		}
+
+	var gotoState = func(st string) {
+		nextfunc = st
 	}
-	var sEnd = func(bFirst bool) {
-		if bFirst {
-			fmt.Println("S_END")
-		}
+	var hasNextState = func() bool {
+		return nextfunc != ""
 	}
-	var sWork = func(bFirst bool) {
-		nextfunc = "S_END"
-	}
+
+	//[STATEGO OUTPUT START] indent(4) $/./$
+    //             psggConverterLib.dll converted from psgg-file:TestControl.psgg
+
+    /*
+        S_0001
+    */
+    var s0001 = func( bFirst  bool ) {
+        var n = 2
+        if n==0 {
+            gotoState( "S_0004" )
+        } else if n==1 {
+            gotoState( "S_0002" )
+        } else if n==2 {
+            gotoState( "S_0003" )
+        }
+    }
+    /*
+        S_0002
+    */
+    var s0002 = func( bFirst  bool ) {
+        if bFirst {
+            fmt.Println("1")
+        }
+        if !hasNextState() {
+            gotoState("S_END")
+        }
+    }
+    /*
+        S_0003
+    */
+    var s0003 = func( bFirst  bool ) {
+        if bFirst {
+            fmt.Println("2")
+        }
+        if !hasNextState() {
+            gotoState("S_END")
+        }
+    }
+    /*
+        S_0004
+    */
+    var s0004 = func( bFirst  bool ) {
+        if bFirst {
+            fmt.Println("0")
+        }
+        if !hasNextState() {
+            gotoState("S_END")
+        }
+    }
+    /*
+        S_END
+    */
+    var sEND = func ( bFirst  bool ) {
+         // end of state machine
+    }
+    /*
+        S_HELLOWORLD
+    */
+    var sHELLOWORLD = func( bFirst  bool ) {
+        if bFirst {
+            fmt.Println("Hello World.")
+        }
+        if !hasNextState() {
+            gotoState("S_NEWERA")
+        }
+    }
+    /*
+        S_NEWERA
+    */
+    var sNEWERA = func( bFirst  bool ) {
+        if bFirst {
+            fmt.Println("We are in the visual programming era.")
+        }
+        if !hasNextState() {
+            gotoState("S_0001")
+        }
+    }
+    /*
+        S_START
+    */
+    var sSTART = func( bFirst  bool ) {
+        gotoState("S_HELLOWORLD")
+    }
+
+
+	//[STATEGO OUTPUT END]
+
 	var funcmap = make(map[string]func(bool))
-	funcmap["S_START"] = S_START
-	funcmap["S_END"] = sEnd
-	funcmap["S_WORK"] = sWork
+	//[STATEGO OUTPUT START] indent(4) $/./->#map$
+    //             psggConverterLib.dll converted from psgg-file:TestControl.psgg
+
+    funcmap["S_0001"] = s0001
+    funcmap["S_0002"] = s0002
+    funcmap["S_0003"] = s0003
+    funcmap["S_0004"] = s0004
+    funcmap["S_END"] = sEND
+    funcmap["S_HELLOWORLD"] = sHELLOWORLD
+    funcmap["S_NEWERA"] = sNEWERA
+    funcmap["S_START"] = sSTART
+
+
+	//[STATEGO OUTPUT END]
 
 	nextfunc = "S_START"
 
@@ -45,22 +137,40 @@ func testControl() {
 	}
 }
 
-// state
-//[STATEGO OUTPUT START] indent(0) $/./$
-//             psggConverterLib.dll converted from psgg-file:TestControl.psgg
+/*  :::: PSGG MACRO ::::
+:psgg-macro-start
 
-//[STATEGO OUTPUT END]
+commentline=// {%0}
 
-//Run
-func run() {
-	// fmt.Println("RUN!")
-	// initialize()
-	// gotoState("S_START")
-	// for checkState("S_END") == false {
-	// 	update()
-	// }
-	// fmt.Println("END OF RUN")
+#map=@@@
+funcmap["[[state]]"] = [[state>>lc]]
+@@@
+
+@branch=@@@
+<<<?"{%0}"/^brif$/
+if [[brcond:{%N}]] {
+    gotoState( "{%1}" )
 }
+>>>
+<<<?"{%0}"/^brifc$/
+if [[brcond:{%N}]] {
+    gotoState( "{%1}" )
+>>>
+<<<?"{%0}"/^brelseif$/
+} else if [[brcond:{%N}]] {
+    gotoState( "{%1}" )
+}
+>>>
+<<<?"{%0}"/^brelseifc$/
+} else if [[brcond:{%N}]] {
+    gotoState( "{%1}" )
+>>>
+<<<?"{%0}"/^brelse$/
+} else {
+    gotoState( "{%1}" )
+}
+>>>
+@@@
 
-//let sm = TestControl()
-//sm.Run()
+:psgg-macro-end
+*/
