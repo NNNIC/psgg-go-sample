3f835b03c1173153239a8d43b7efd917a42b5474
nn4343 <gea01441@nifty.com>
2020-11-14 14:18:43 +0900

    no message

--- test-ebiten/testc/main.go
+++ test-ebiten/testc/main.go
@@ -1,6 +1,8 @@
 // Original Copyright 2014 Hajime Hoshi
 package main
-
+import {
+    "github.com/NNNIC/psgg-go-sample/test-ebiten/testc/mainControl"
+}
 func main() {
-	Maingame()
+	mainControl.Maingame()
 }
