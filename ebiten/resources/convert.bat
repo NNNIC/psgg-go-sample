c3a9be8903a74df755029015ddcac03619c48da7
nn4343 <gea01441@nifty.com>
2020-11-21 10:49:42 +0900

    no message

--- ebiten/resources/convert.bat
+++ ebiten/resources/convert.bat
@@ -5,6 +5,11 @@ set TL=file2byteslice-master\cmd\file2byteslice\file2byteslice.exe
 %TL% -input mascot_t32.png -output sgimg/mascot32.go -package sgimg -var Mascot32_png
 %TL% -input mascot_t64.png -output sgimg/mascot64.go -package sgimg -var Mascot64_png
 %TL% -input mascot_t128.png -output sgimg/mascot128.go -package sgimg -var Mascot128_png
-%TL% -input mascot_t256.png -output sgimg/mascot256.go -package sgimg -var Mascot1256_png
+%TL% -input mascot_t256.png -output sgimg/mascot256.go -package sgimg -var Mascot256_png
+
+%TL% -input gopher_t32.png -output sgimg/gopher32.go -package sgimg -var Gopher32_png
+%TL% -input gopher_t64.png -output sgimg/gopher64.go -package sgimg -var Gopher64_png
+%TL% -input gopher_t128.png -output sgimg/gopher128.go -package sgimg -var Gopher128_png
+
 
 pause
