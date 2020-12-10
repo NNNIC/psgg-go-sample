125677bcf612935f908ea9fd647366c512e18d06
nn4343 <gea01441@nifty.com>
2020-12-10 22:24:00 +0900

    no message

--- ebiten/sample/_run.bat
+++ ebiten/sample/_run.bat
@@ -1,6 +1,6 @@
-:: 1. with testControl.go
-::go run testControl.go game.go mainControl.go  gameFont.go main.go
-::
-:: 2. without testControl.go
-go run game.go mainControl.go  gameFont.go main.go
+if exist testControl.go (
+go run testControl.go game.go mainControl.go  gameFont.go main.go
+) else (
+    go run game.go mainControl.go  gameFont.go main.go
+)
 pause
