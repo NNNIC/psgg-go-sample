b34ea4a328a5b3cc87e381b45892791c854655b1
nn4343 <gea01441@nifty.com>
2020-12-10 22:14:01 +0900

    no message

--- ebiten/sample/_run.bat
+++ ebiten/sample/_run.bat
@@ -1,1 +1,6 @@
-go run game.go mainControl.go testControl.go gameFont.go main.go
+:: 1. with testControl.go
+::go run testControl.go game.go mainControl.go  gameFont.go main.go
+::
+:: 2. without testControl.go
+go run game.go mainControl.go  gameFont.go main.go
+pause
